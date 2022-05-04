62d4205e8cb3630c679c08766af2a6b19de904a4
aecesr <1725942140@qq.com>
2022-05-05 00:57:50 +0800

    feat: 左侧菜单伸缩功能实现

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- 一级 menu 菜单 -->
   <el-menu
+    :collapse="!$store.getters.sidebarOpened"
     :default-active="activeMenu"
     :background-color="$store.getters.cssVar.menuBg"
     :text-color="$store.getters.cssVar.menuText"
@@ -14,7 +15,7 @@
 <script setup>
 import SidebarItem from './SidebarItem.vue'
 import { computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 import { filterRouters, generateMenus } from '@/utils/route'
 
 const router = useRouter()
