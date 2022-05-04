7f8d6ad3a50f549ef4665f5f833376c23094a533
aecesr <1725942140@qq.com>
2022-05-05 00:04:09 +0800

    feat: 修复菜单中残留问题

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -1,11 +1,12 @@
 <template>
   <!-- 一级 menu 菜单 -->
   <el-menu
-    :uniqueOpened="true"
-    default-active="2"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
+    :default-active="activeMenu"
+    :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText"
+    :active-text-color="$store.getters.cssVar.menuActiveText"
+    :unique-opened="true"
+    router
   >
     <sidebar-item v-for="item in routes" :key="item.path" :route="item"></sidebar-item>
   </el-menu>
@@ -22,4 +23,10 @@ const routes = computed(() => {
   return generateMenus(filterRoutes)
 })
 console.log(JSON.stringify(routes.value))
+// 计算高亮menu的方法
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
+})
 </script>
