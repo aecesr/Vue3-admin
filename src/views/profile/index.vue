c79c84f505ba3cf7e528873590215e48114ecf49
aecesr <1725942140@qq.com>
2022-05-07 00:19:49 +0800

    feat: 个人中心模块基本布局

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,65 +1,41 @@
 <template>
-  <el-row>
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </el-row>
-  <div id="chart-one" ref="chart" style="height: 500px; width: 500px"></div>
+  <div class="my-container">
+    <el-row>
+      <el-col :span="6">
+        <project-card class="user-card"></project-card>
+      </el-col>
+      <el-col :span="18">
+        <el-card>
+          <el-tabs v-model="activeName">
+            <el-tab-pane :label="$t('msg.profile.feature')" name="feature">
+              <feature />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.chapter')" name="chapter">
+              <chapter />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.author')" name="author">
+              <author />
+            </el-tab-pane>
+          </el-tabs>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<script>
-// import Echarts from 'echarts' // 引入Echarts插件
-const Echarts = require('echarts')
-// 按需引入对应的模块
-// require('echarts/lib/chart/bar')
-
-export default {
-  data() {
-    return {
-      chart: null
-    }
-  },
-  // 挂在Echarts实例的dom节点
-  mounted() {
-    // 调用方法绘制图表
-    this.initChart()
-    // 自适应窗口大小
-    window.addEventListener('resize', () => {
-      this.chart.resize()
-    })
-  },
-  methods: {
-    initChart() {
-      // 初始化Echarts实例
-      // 使用id的方式绑定容器时使用
-      console.log(Echarts)
-      // this.chart = Echarts.init(document.getElementById('chart-one'))
-      console.log(this.chart)
-      // 使用ref绑定图表放置的容器时使用
-      this.chart = Echarts.init(this.$refs.chart)
-      // 绘制图表
-      const option = {
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            data: [150, 230, 224, 218, 135, 147, 200],
-            type: 'line'
-          }
-        ]
-      }
-      this.chart.setOption(option)
-    }
-  }
-}
+<script setup>
+import ProjectCard from './components/ProjectCard.vue'
+import Chapter from './components/Chapter.vue'
+import Feature from './components/Feature.vue'
+import Author from './components/Author.vue'
+import { ref } from 'vue'
+const activeName = ref('feature')
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.my-container {
+  .user-card {
+    margin-right: 20px;
+  }
+}
+</style>
